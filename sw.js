0e87c48798f66107f098826788cf6a3350124413
guilhermefernandesTI <gs667680@gmail.com>
2026-08-19 19:39:26 -0300

    Atualiza cache e remove versão antiga

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "agenda-barbearia-v11";
+const cacheName = "agenda-barbearia-v12";
 
 const assets = [
   "./",
@@ -13,21 +13,37 @@ const assets = [
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))),
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== cacheName)
+            .map((key) => caches.delete(key))
+        )
+      )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  // Não intercepta as requisições da API
   if (event.request.url.includes("/api/")) {
     return;
   }
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
 });
